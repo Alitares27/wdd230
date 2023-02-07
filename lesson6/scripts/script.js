b16bca8e18c07ec1c5f2724e7e30030f000f124a
AldairPay <pay22002@byui.edu>
2023-02-07 17:23:13 -0500

    updating

---
+++ lesson6/scripts/script.js
@@ -0,0 +1,56 @@
+let date = new Date(document.lastModified);
+let shortDate = date.toLocaleDateString('en-US');
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let fullDate = `${shortDate} ${hour}:${minutes}:${seconds}`
+document.getElementById('modifiedDate').innerHTML = fullDate;
+let currentDate = new Date()
+let currentYear = currentDate.getFullYear()
+document.getElementById('year').innerHTML = currentYear;
+
+const daynames = [
+	'Sunday',
+	'Monday',
+	'Tuesday',
+	'Wednesday',
+	'Thursday',
+	'Friday',
+	'Saturday'
+];
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const newdate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.getElementById('date-box').innerHTML = newdate
+
+function togglemenu(){
+document.getElementById('primaryNav').classList.toggle('open');
+document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+const x = document.getElementById('hamburgerBtn')
+x.onclick = togglemenu;
+
+const day = new Date().getDay(); 
+let message;
+if (day > 0 && day < 3 ) {
+  message = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+} else {
+  message = '';
+}
+document.getElementById("message").innerHTML = message;
