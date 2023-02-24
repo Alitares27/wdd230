31c06199e474d3bd0424c7019efff93d891ab218
AldairPay <pay22002@byui.edu>
2023-02-24 00:58:27 -0500

    updating

--- chamber/scripts/app.js
+++
@@ -1,8 +0,0 @@
-function togglemenu(){
-    document.getElementById('primaryNav').classList.toggle('open');
-    document.getElementById('hamburgerBtn').classList.toggle('open');
-    }
-    const x = document.getElementById('hamburgerBtn')
-    x.onclick = togglemenu;
-    
-    
