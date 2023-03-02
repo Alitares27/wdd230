04ceb766818fc711ba418e0738b81a28ab9cfd8c
AldairPay <pay22002@byui.edu>
2023-03-02 01:42:43 -0500

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,15 +2,15 @@ const url = 'https://alitares27.github.io/wdd230/chamber/scripts/data.json';
 async function getData() {
     const response = await fetch(url);
     const data = await response.json();
+    displayData(data.companies);
     console.table(data.companies);  // note that we reference the prophet array of the data object given the structure of the json file
   }
-  
   getData();
 
 //**Display Function**/
 const displayData = (companies) => {
-    const cards = document.getElementById('container');
-    company.forEach((resti) => {
+    const cards = document.querySelector('.cards');
+    companies.forEach((company) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let portrait = document.createElement('img');
@@ -21,20 +21,20 @@ const displayData = (companies) => {
        
         h1.textContent = `${company.name}`;
         h4.textContent = `Category: ${company.category}`;
-        h41.textContent = `Website: ${resti.Website}`;
-        p.textContent = `Description: ${resti.Description}`;
+        h41.textContent = `Website: ${company.Website}`;
+        p.textContent = `Description: ${company.Description}`;
         
-        portrait.setAttribute('src', resti.image);
-        portrait.setAttribute('alt', `Portait of ${res.name}`);
+        portrait.setAttribute('src', company.image);
+        portrait.setAttribute('alt', `Portait of ${company.name}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
-        card.appendChild('h1');
-        card.appendChild('portrait');
-        card.appendChild('h4');
-        card.appendChild('h41');
-        card.appendChild('p');
+        card.appendChild(h1);
+        card.appendChild(portrait);
+        card.appendChild(h4);
+        card.appendChild(h41);
+        card.appendChild(p);
 
         cards.appendChild(card);
     })
