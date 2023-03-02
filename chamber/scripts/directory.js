1f2d79ecc1a383b14156d9bbfd61370dddbde2d7
AldairPay <pay22002@byui.edu>
2023-03-02 01:00:47 -0500

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,16 +1,16 @@
 const url = 'https://alitares27.github.io/wdd230/chamber/scripts/data.json';
-async function getProphetData() {
+async function getData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+    console.table(data.companies);  // note that we reference the prophet array of the data object given the structure of the json file
   }
   
-  getProphetData();
+  getData();
 
 //**Display Function**/
-const displayData = (res) => {
-    const cards = document.get('.container');
-    res.forEach((resti) => {
+const displayData = (companies) => {
+    const cards = document.getElementById('container');
+    company.forEach((resti) => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let portrait = document.createElement('img');
@@ -19,8 +19,8 @@ const displayData = (res) => {
         let h41 = document.createElement('h4')
         let p = document.createElement('p')
        
-        h1.textContent = `${resti.name}`;
-        h4.textContent = `Category: ${resti.category}`;
+        h1.textContent = `${company.name}`;
+        h4.textContent = `Category: ${company.category}`;
         h41.textContent = `Website: ${resti.Website}`;
         p.textContent = `Description: ${resti.Description}`;
         
