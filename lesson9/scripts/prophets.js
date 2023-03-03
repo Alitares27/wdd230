95569e6535289ebcba97f5c04cd35d46a824c4d8
AldairPay <pay22002@byui.edu>
2023-03-02 23:10:23 -0500

    updating

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -22,6 +22,9 @@ async function getProphetData() {
       let h32 = document.createElement('h3')
       let h1 = document.createElement('h1')
       let h33 = document.createElement('h3')
+      let h34 = document.createElement('h3')
+      
+ 
  
       // Build the h2 content out to show the prophet's full name - finish the template string
       h2.textContent = `${prophet.name} ${prophet.lastname}`;
@@ -32,12 +35,23 @@ async function getProphetData() {
       h1.textContent = `${prophet.order}° President`;
 
       //dates
+      var nage = new Date(prophet.death)
+      var nage2 = new Date(prophet.birthdate)
+      console.log(nage)
+      if(prophet.death === null){
+        console.log(prophet.name)
+        h33.style.display = 'none';
+        h32.style.display = 'none';
+        h34.textContent = 'He is our current prophet';
+        h34.style.fontSize = '200%'
+      }
+      
       bdate = new Date(prophet.birthdate)
       ddeath = new Date(prophet.death)
       let dif = ddeath.getYear() - bdate.getYear()
       h33.textContent ='Age: '+ dif;
       
-      //Build the h2 content
+      
   
       // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', prophet.imageurl);
@@ -53,6 +67,7 @@ async function getProphetData() {
       card.appendChild(h3);
       card.appendChild(h32);
       card.appendChild(h33);
+      card.appendChild(h34);
       card.appendChild(portrait);
       cards.appendChild(card);
     })// end of forEach loop
