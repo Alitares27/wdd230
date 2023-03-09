4951d535aafa868d3397f8209e19842f8ed377ca
AldairPay <pay22002@byui.edu>
2023-03-08 22:44:52 -0500

    updating wind chill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,34 +1,58 @@
+const Wimg = document.querySelector('.weather-img');
 const speed = document.querySelector('.speed')
 const grade = document.querySelector('.grade')
 const chill = document.querySelector('.chill')
+const desc = document.querySelector('.desc')
 
 //Temperature
-function randomTemperature(min, max) {
-    min = -20;
-    max = 10;
-    return Math.round(Math.floor((Math.random() * (max - min + 1)) + min));
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-16.399&lon=-71.535&appid=4459620e17f02a15afac1eef17a40f98';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); 
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function  displayResults(weatherData) {
+    kelvin = weatherData.main.temp.toFixed(0);
+    temperature = kelvin - 273.15
+    grade.innerHTML = temperature.toFixed(0);
+
+    //velocity
+    const vel = weatherData.wind.speed;
+    speed.innerHTML= vel;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const descript = (weatherData.weather[0].description).toUpperCase();
+  
+    Wimg.setAttribute('src', iconsrc);
+    Wimg.setAttribute('alt', desc);
+    desc.textContent = descript;
+if (temperature>11 || vel > 4){
+    chill.innerHTML = 'N/A';
+}else{
+    const Wchill = ((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(vel,0.16)) + (0.4275*temperature*Math.pow(vel,0.16))).toFixed(1);
+    console.log(Wchill)
+    chill.innerHTML = Wchill;
 }
-const temperature = randomTemperature()
 
-//Velocity
-function randomspeed(min, max) {
-    min = 4.8;
-    max = 177;
-    return (Math.floor((Math.random() * (max - min + 1)) + min)).toFixed(1);
-}
-const velocity = randomspeed()
+  }
 
-//converter
-var mph = (velocity / 1.609).toFixed(1)
-var celsius= temperature;
-var tempfahrenheit  = Math.round((celsius * (9/5))+32)
 
-//Calculator
-var fahrenheitChill = ((35.74 + (0.6215 * tempfahrenheit))-(35.75 * Math.pow(mph,0.16)) + (0.4275*tempfahrenheit*Math.pow(mph,0.16))).toFixed(2);
-var celsiuschill = ((fahrenheitChill-32)*(5/9)).toFixed(1)
 
-//printer
-chill.innerHTML = celsiuschill
-grade.innerHTML = temperature
-speed.innerHTML = velocity
+
+
+
+
 
